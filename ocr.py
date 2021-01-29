5f1bf5151e464c1bc3aa23987b3fe60cee94ac23
SJH <nnocreater@hotmail.com>
2021-01-29 16:00:47 +0800

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -9,6 +9,8 @@ ocr使用百度的接口，如果需要更换请保持general_ocr函数名、参
 """
 
 # 加载 api key 和 secret key
+_API_KEY = ""
+_SECRET_KEY = ""
 try:
     with open('config123.json', 'r') as config:
         jsons = json.loads(config.read())
@@ -28,8 +30,6 @@ def _get_token() -> str:
     # client_id 为官网获取的AK， client_secret 为官网获取的SK
     host = 'https://aip.baidubce.com/oauth/2.0/token' \
            f'?grant_type=client_credentials&client_id={_API_KEY}&client_secret={_SECRET_KEY}'
-    print(_API_KEY)
-    print(_SECRET_KEY)
     response = requests.get(host)
     if response.ok:
         return response.json().get('access_token')
