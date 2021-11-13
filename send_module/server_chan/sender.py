59814091ecb558a53c561830a743999e7eb0d19e
TsinbeiTech <team@tsinbei.com>
2021-11-13 16:53:44 +0800

    Update sender.py

--- send_module/server_chan/sender.py
+++ send_module/server_chan/sender.py
@@ -3,28 +3,21 @@ import requests
 sess = requests.session()
 send_key = ""
 
-def set_access_token(token):
-    global access_token
-    access_token = token
 
-def set_api_url(url):
-    global api_url
-    api_url = url
+def set_key(key):
+    global send_key
+    send_key = key
 
-def set_user_id(id):
-    global user_id
-    user_id = id
 
 def send(title, content) -> dict:
-    resp = sess.post(url=f"{api_url}", data={
-        'access_token': f"{access_token}",
-        'user_id': f"{user_id}",
-        'message': f"# {title}\n\n{content}"
+    resp = sess.post(url=f"https://sctapi.ftqq.com/{send_key}.send", data={
+        'text': f"{title}",
+        'desp': f"# {title}\n\n{content}"
     })
     res = resp.json()
-    success = res['status'] == 'ok'
+    success = res['code'] == 0
     return {
         'success': success,
-        'message': res['status'] if not success else '发送成功'
+        'message': res['message'] if not success else '发送成功'
     }
 
