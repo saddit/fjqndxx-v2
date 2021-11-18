fd35e8b40e3aa0b86b6498a5562a0eff2949d5ea
TsinbeiTech <team@tsinbei.com>
2021-11-18 22:37:09 +0800

    Update sender.py

--- send_module/qqbot/sender.py
+++ send_module/qqbot/sender.py
@@ -2,22 +2,30 @@ import requests
 
 sess = requests.session()
 send_key = ""
+user_id = ""
+api_url = ""
 
+def set_api_url(url):
+    global api_url
+    api_url = url
 
 def set_key(key):
     global send_key
     send_key = key
 
+def set_user_id(id):
+    global user_id
+    user_id = id
 
 def send(title, content) -> dict:
-    resp = sess.post(url=f"https://sctapi.ftqq.com/{send_key}.send", data={
-        'text': f"{title}",
-        'desp': f"# {title}\n\n{content}"
+    resp = sess.post(url=f"{api_url}/send_private_msg?access_token={send_key}", data={
+        'user_id': f"{user_id}",
+        'message': f"# {title}\n\n{content}"
     })
     res = resp.json()
-    success = res['code'] == 0
+    success = res['status'] == "ok"
     return {
         'success': success,
-        'message': res['message'] if not success else '发送成功'
+        'message': res['msg'] if not success else '发送成功'
     }
 
