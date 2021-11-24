940cdaaede456873579ab11c917ea058098fac57
TsinbeiTech <team@tsinbei.com>
2021-11-24 17:30:33 +0800

    Update sender.py

--- send_module/qqbot/sender.py
+++ send_module/qqbot/sender.py
@@ -4,7 +4,8 @@ sess = requests.session()
 user_id = ""
 access_token = ""
 api_url = ""
-
+group_id = ""
+at_user = false
 def set_api_url(url):
     global api_url
     api_url = url
@@ -13,6 +14,14 @@ def set_access_token(token):
     global access_token
     access_token = token
 
+def set_group_id(id):
+    global group_id
+    group_id = id
+    
+def set_at_user(type):
+    global at_user
+    at_user = type
+    
 def set_user_id(id):
     global user_id
     user_id = id
