fbb6cca3138086234a75a1251cdaacf1f692956a
Dark-Exsited <c2572388311@gmail.com>
2022-06-25 00:38:38 +0800

    fix: docker not works

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-FROM python:3.9-alpine
+FROM python:3.9.13-bullseye
+# FROM python:3.9-alpine
 
 # run on evety Monday,Tuesday at 9:00
 ENV CRONTIME="0 9 * * 1,4"
 ENV username=
 ENV password=
 ENV pubKey=A7E74D2B6282AEB1C5EA3C28D25660A7
-# ocr：baidu_image
+# ocr：baidu_image, tesseract
 ENV ocrType=
 ENV ocrSecret=
 ENV ocrKey=
@@ -17,18 +18,12 @@ ENV sendMode=
 ENV extUsers=
 ENV TZ=Asia/Shanghai
 
-RUN apk update; \
-    apk add tzdata git gcc libc-dev libxml2-dev libxslt-dev;
 
-RUN cp /usr/share/zoneinfo/${TZ} /etc/localtime \
-    && echo ${TZ} > /etc/timezone
+RUN apt update && apt install -y cron tesseract-ocr libtesseract-dev libleptonica-dev
 
-RUN git clone https://github.com/838239178/tk-auto-study.git /root/tk-auto-study \
-    && cd /root/tk-auto-study \
-    && git checkout master; \
-    pip install -r requirements.txt;
+ADD ./ /root/tk-auto-study/ 
+RUN cd /root/tk-auto-study && pip install -r requirements.txt
 
-RUN crontab -l | { cat; echo "$CRONTIME python /root/tk-auto-study/docker.py"; } | crontab -; \
-    crond -b -l 8;
+RUN crontab -l | { cat; echo "$CRONTIME python /root/tk-auto-study/docker.py"; } | crontab -;
 
-CMD ["tail", "-f", "/dev/null"]
+CMD ["cron", "-f"]
--- ocr_module/tesseract/tesseract_ocr.py
+++ ocr_module/tesseract/tesseract_ocr.py
@@ -1,6 +1,10 @@
 import os
 import platform
 import subprocess
+from PIL import Image
+from io import BytesIO
+import base64
+import re
 
 SYSTEM_PLATFORM = platform.system()
 THIS_PATH = os.path.dirname(__file__)
@@ -8,17 +12,22 @@ THIS_PATH = os.path.dirname(__file__)
 def is_need_keys() -> bool:
     return False
 
-
 def get_result(img: bytes) -> str:
     if SYSTEM_PLATFORM == 'Linux':
-        exe = os.path.join(THIS_PATH, "ocr-x86_64-linux-gnu")
+        import pytesseract
     elif SYSTEM_PLATFORM == 'Windows':
         exe = os.path.join(THIS_PATH, "ocr-x86_64-windows-msvc.exe")
     else:
-        raise Exception(f"tesseract 不支持该操作系统{SYSTEM_PLATFORM}, 仅支持Windows或Linux")
-    
+        raise Exception(f"tesseract 不支持该操作系统{SYSTEM_PLATFORM}, 仅支持 Windows 或 Linux")
     try:
-        out_bytes = subprocess.check_output([exe, '--validate-code-base64', img.decode()])
-        return out_bytes.decode()
+        if (SYSTEM_PLATFORM == 'Windows'):
+            out_bytes = subprocess.check_output([exe, '--validate-code-base64', img.decode()])
+            return out_bytes.decode()
+        elif (SYSTEM_PLATFORM == 'Linux'):
+            digit_pattern = re.compile(r'\d+')
+            image = Image.open(BytesIO(base64.b64decode(img.decode())))
+            out_bytes = pytesseract.image_to_string(image, lang='eng', config='--psm 7 --oem 3 -c tessedit_char_whitelist=0123456789')
+            out_bytes = digit_pattern.findall(out_bytes)
+        return out_bytes[0]
     except subprocess.CalledProcessError as e:
         raise Exception(f"ocr 识别失败，错误代码{e.returncode}")
