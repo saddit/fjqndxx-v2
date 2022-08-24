0d506a3c4cbf211baf5a50a735a4c558d1085eeb
Shijh <a838239178@gmail.com>
2022-08-24 22:02:00 +0800

    feat: max retry add to config

--- util.py
+++ util.py
@@ -1,3 +1,9 @@
 
 def is_set(val) -> bool:
-    return val is not None and val != "" and len(val) > 0
+    if isinstance(val, int):
+        return val is not None
+    if isinstance(val, str):
+        return val is not None and val != ""
+    if isinstance(val, list):
+        return val is not None and len(val) > 0
+    return val is not None
