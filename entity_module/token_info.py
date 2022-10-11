16549d7e75a0224b4daf4f3b18e3f7ad26d2d479
Shijh <a838239178@gmail.com>
2022-10-11 23:28:35 +0800

    feat:  token_info init logic

--- entity_module/token_info.py
+++ entity_module/token_info.py
@@ -9,12 +9,12 @@ class TokenInfo(object):
     expire_at: float
 
     def __init__(self, dt: dict = {}) -> None:
-        self.refresh_expire = dt.get('refreshExpire', 0)
-        self.refresh_token = dt.get('refreshToken', '')
+        self.refresh_expire = dt.get('refreshExpire', dt.get('refresh_expire', 0))
+        self.refresh_token = dt.get('refreshToken', dt.get('refresh_token', ''))
         self.token = dt.get('token', '')
         self.expire = dt.get('expire', 0)
         ts = (datetime.now() + timedelta(seconds=self.expire)).timestamp()
         self.expire_at = dt.get('expire_at', ts)
-        
+
     def get_expired_at(self) -> datetime:
         return datetime.fromtimestamp(self.expire_at)
